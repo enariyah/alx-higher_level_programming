8e61ad7d9e0de541bdc3e77bdb742c6f4b932677
enariyah <hopmichael52@gmail.com>
2023-03-06 22:48:39 +0300

    fix

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-sys.stderr.write(message)
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
